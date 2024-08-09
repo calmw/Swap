c793fa910e0b1aef6299a528405e44257c5725b8
calmw <calm.wang@hotmail.com>
2024-08-09 10:47:21 +0800

    移除流动性

--- contracts/Tox.sol
+++ contracts/Tox.sol
@@ -6,7 +6,7 @@ import "./RoleAccess.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ToxA is RoleAccess, ERC20 {
-    constructor(uint256 initialSupply) ERC20("INTOVERSE TOKEN", "TOXA") {
+    constructor(uint256 initialSupply) ERC20("Exchange TOKEN", "EX") {
         _addAdmin(msg.sender);
         _mint(msg.sender, initialSupply);
     }
